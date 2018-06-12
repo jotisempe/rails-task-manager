d9b80e7033ebee943c276964819f34f367878b3d
joti sempe <josefinasempe@gmail.com>
2018-06-12 15:12:41 -0300

    CRUD done. views and controllers done.(confirmation to delete) NO CSS yet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,16 @@
 class TasksController < ApplicationController
 
+ before_action :set_task, only: [:show, :edit, :update, :destroy]
 #create
+def new
+  @task = Task.new
+end
+
+def create
+  @task = Task.new(task_params)
+  @task.save
+  redirect_to task_path(@task)
+end
 
 #read
 def index
@@ -8,12 +18,34 @@ def index
 end
 
 def show
+end
+
+#update
+def edit
+end
+
+def update
+  @task.update(task_params)
+  redirect_to task_path(@task)
+end
+
+#destroy
+def destroy
+  @task.destroy
+  redirect_to tasks_path
+end
+
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
+end
+
+def set_task
   @task = Task.find(params[:id])
 end
 
 
-#update
-
-#destroy
 
 end
